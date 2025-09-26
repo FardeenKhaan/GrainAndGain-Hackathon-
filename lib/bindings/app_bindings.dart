b81cf078316583ac0e2997690496615954bbed4a
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-09-26 23:17:40 +0500

    created for controllers

--- lib/bindings/app_bindings.dart
+++ lib/bindings/app_bindings.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 import 'package:grain_and_gain_student/controllers/auth_controller.dart';
+import 'package:grain_and_gain_student/controllers/task_controller.dart';
+import 'package:grain_and_gain_student/controllers/wallet_controller.dart';
 
 class AppBindings extends Bindings {
   @override
   void dependencies() {
-    // Get.put(AuthController());
-    Get.lazyPut<AuthController>(() => AuthController());
+    Get.put(AuthController(), permanent: true);
+    // Get.lazyPut<AuthController>(() => AuthController());
+    Get.put(WalletController(), permanent: true);
+    Get.put(TaskController(), permanent: true);
   }
 }
