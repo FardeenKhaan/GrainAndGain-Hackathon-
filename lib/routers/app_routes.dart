0bec3fc260b3ad218ed41f03d59f454210d52c31
Fardeen Khan <fardeenkhaan70@gmail.com>
2025-10-11 22:44:03 +0500

    updated routing flow

--- lib/routers/app_routes.dart
+++ lib/routers/app_routes.dart
@@ -2,14 +2,14 @@ import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:grain_and_gain_student/routers/routes.dart';
 import 'package:grain_and_gain_student/screens/auth/login_view.dart';
 import 'package:grain_and_gain_student/screens/auth/signup_view.dart';
-import 'package:grain_and_gain_student/screens/dashboards/restaurant_owner_dashboard.dart';
-import 'package:grain_and_gain_student/screens/dashboards/student_dashboard.dart';
+import 'package:grain_and_gain_student/screens/dashboards/restaurant_dashboard/restaurant_owner_dashboard.dart';
+import 'package:grain_and_gain_student/screens/dashboards/student_dashboard/student_dashboard.dart';
 
 class AppRoutes {
   static final pages = [
     GetPage(name: FkRoutes.logIn, page: () => LoginView()),
     GetPage(name: FkRoutes.signUp, page: () => SignupView()),
-    GetPage(name: FkRoutes.dashboard, page: () => DashboardView()),
+    GetPage(name: FkRoutes.studentDashboard, page: () => StudentDashboardView()),
     GetPage(name: FkRoutes.restaurantDashboard, page: () => RestaurantDashboardView()),
   ];
 }
